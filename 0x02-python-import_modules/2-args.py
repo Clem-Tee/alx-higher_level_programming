4d08faf8a78dc01d47f42fe4e43b66660527099d
Clem-Tee <clementogol@gmail.com>
2021-11-18 22:05:28 +0300

    Added mandatory task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
-                                 "." if l == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+    import sys
+    l = len(sys.argv) - 1
+    print("{} argument{}{}"
+          .format(l, "" if l == 1 else "s", "." if l == 0 else ":"))
+    for i in range(l):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
