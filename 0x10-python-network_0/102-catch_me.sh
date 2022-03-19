7beee4464a3c040afb5a924e95e0c8cdf3cfbcc8
Clem-Tee <clementogol@gmail.com>
2022-03-19 12:29:44 +0300

    Updated file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# catch as catch can
-curl -sL -X PUT -H "You got me!" -d "user_id=98" "0.0.0.0:5000/catch_me"
+# Catch me
+curl "0.0.0.0:5000/catch_me" -sLX PUT -H "Origin: School" -d "user_id=98"
